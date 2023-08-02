973de7448d438d85e725b193906b7d692b6ec94e
Moritz Kirstein <mkirstein@protonmail.com>
2023-08-02 17:02:28 +0200

    refactor: edit links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/deltaDAO/pontus-x-docs',
+            'https://github.com/deltaDAO/pontus-x-docs/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/deltaDAO/pontus-x-docs',
+            'https://github.com/deltaDAO/pontus-x-docs/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
