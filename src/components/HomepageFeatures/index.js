f66a1dd865838db7c2418a70269fa2a7b83de830
Luca Milanese <luca.milanese90@gmail.com>
2023-08-10 17:46:26 +0200

    feat: update hero section and feature list

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,52 +1,68 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from 'react'
+import clsx from 'clsx'
+import Link from '@docusaurus/Link'
+import styles from './styles.module.css'
 
 const FeatureList = [
-  // {
-  //   title: 'Easy to Use',
-  //   Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-  //   description: (
-  //     <>
-  //       Docusaurus was designed from the ground up to be easily installed and
-  //       used to get your website up and running quickly.
-  //     </>
-  //   ),
-  // },
-  // {
-  //   title: 'Focus on What Matters',
-  //   Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-  //   description: (
-  //     <>
-  //       Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-  //       ahead and move your docs into the <code>docs</code> directory.
-  //     </>
-  //   ),
-  // },
-  // {
-  //   title: 'Powered by React',
-  //   Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-  //   description: (
-  //     <>
-  //       Extend or customize your website layout by reusing React. Docusaurus can
-  //       be extended while reusing the same header and footer.
-  //     </>
-  //   ),
-  // },
-];
+  {
+    title: 'Blog',
+    Svg: require('@site/static/img/rss-feed.svg').default,
+    description: (
+      <>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum dolorem
+        error commodi exercitationem? Rem illo, recusandae.
+      </>
+    ),
+    action: {
+      title: 'Visit our Blog',
+      link: '/blog',
+    },
+  },
+  {
+    title: 'Docs',
+    Svg: require('@site/static/img/library-books.svg').default,
+    description: (
+      <>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum dolorem
+        error commodi exercitationem? Rem illo, recusandae.
+      </>
+    ),
+    action: {
+      title: 'Start here',
+      link: '/docs/intro',
+    },
+  },
+  {
+    title: 'Github',
+    Svg: require('@site/static/img/github-mark.svg').default,
+    description: (
+      <>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum dolorem
+        error commodi exercitationem? Rem illo, recusandae.
+      </>
+    ),
+    action: {
+      title: 'Contribute',
+      link: 'https://github.com/deltaDAO/pontus-x-docs',
+    },
+  },
+]
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description, action }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx('col col--4', styles.feature)}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
         <p>{description}</p>
+        <Link className="button button--secondary button--md" to={action.link}>
+          {action.title}
+        </Link>
       </div>
     </div>
-  );
+  )
 }
 
 export default function HomepageFeatures() {
@@ -60,5 +76,5 @@ export default function HomepageFeatures() {
         </div>
       </div>
     </section>
-  );
+  )
 }
